bd646e6e6e36a4fc677ebb4cfd0604c29bde887d
ssulphurr <ssulphurr@gmail.com>
2023-04-21 15:05:35 +0300

    adds some css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ export const Statistics = ({ title, stats }) => {
         <ul className={css.stat_list}>
           {stats.map(stat => (<li key={stat.id} className={css.item}>
             <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>))}
         </ul>
       </section>
