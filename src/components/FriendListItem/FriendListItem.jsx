ab70b58bf3d16df1071cd4dfff0b0b5b4936c9d3
ssulphurr <ssulphurr@gmail.com>
2023-04-21 18:54:05 +0300

    comments propTypes in FriendsListItem to fix error

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,4 @@
-import { PropTypes } from 'prop-types';
+// import { PropTypes } from 'prop-types';
 import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
