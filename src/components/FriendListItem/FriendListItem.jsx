a55f5ee856635981ac54dfad3aad5e9b255cfb4b
ssulphurr <ssulphurr@gmail.com>
2023-04-21 19:00:40 +0300

    removes key from FriendsListItem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
   return (
-    <li key={id} className={css.item}>
+    <li className={css.item}>
       <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
