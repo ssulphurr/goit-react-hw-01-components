8c90b253a506948b21395bb44a10ead78030efc0
ssulphurr <ssulphurr@gmail.com>
2023-04-21 14:42:44 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,21 +3,18 @@ import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
   return (
-    <section className={css.statistics}>
-      {title && <h2 className={css.title}>{title}</h2>}
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
 
-      <ul className={css.stat_list}>
-       {stats.map(stat => ( 
-          <li key={stat.id} className={css.item}>
-                <span className={css.label}>{ stat.label}</span>
-           <span className={css.percentage}>{stat.percentage}</span>
-           
-           {/* спробувати через акумулятор */}
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+        <ul className={css.stat_list}>
+          {stats.map(stat => (<li key={stat.id} className={css.item}>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}</span>
+          </li>))}
+        </ul>
+      </section>
+ 
+    );
 };
 
 Statistics.propTypes = {
@@ -28,3 +25,7 @@ Statistics.propTypes = {
     percentage: PropTypes.number,
   }))
 }
+
+// function randomColor() {
+//   Math.floor(Math.random() * 16777215).toString(16);
+// }
