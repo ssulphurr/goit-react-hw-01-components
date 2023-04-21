92178d997606d5888caf79510bb1d66f2ce40887
ssulphurr <ssulphurr@gmail.com>
2023-04-21 13:15:26 +0300

    adds task 3

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,14 @@
+import { PropTypes } from 'prop-types'
+import css from './FriendList.module.css'
+
+export const FriendList = ({ friends }) => {
+    return <ul className={css.friend_list}>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+            <li key={id} className={css.item}>
+                <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+                <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+                <p className={css.name}>{name}</p>
+            </li>
+        ))}
+    </ul>
+}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,7 +10,9 @@ export const Statistics = ({ title, stats }) => {
        {stats.map(stat => ( 
           <li key={stat.id} className={css.item}>
                 <span className={css.label}>{ stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}</span>
+           <span className={css.percentage}>{stat.percentage}</span>
+           
+           {/* спробувати через акумулятор */}
           </li>
         ))}
       </ul>
